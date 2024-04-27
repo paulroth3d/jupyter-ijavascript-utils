6abc285f5806e0033e897b967419de4765ffdbb2
paulroth3d <paulroth3d@gmail.com>
2024-04-27 12:21:25 -0500

    IMPORT: initial single webpack version

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/webpack.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'jupyter-ijavascript-utils.js'
+  },
+  resolve: {
+    fallback: {
+      "fs": false,
+      "path": false,
+      "stream": false,
+      "buffer": false,
+      "constants": false,
+      "util": false,
+      "events": false,
+      "assert": false
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
+
+module.exports = config;
